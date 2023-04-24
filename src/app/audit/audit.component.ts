569cb1c9578276d9a6b61057821200ccb8aab9cd
Manjiree <manjireepatil08@outlook.com>
2023-04-25 03:43:32 +0530

    Table added with date format

--- src/app/audit/audit.component.ts
+++ src/app/audit/audit.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 
-import { Audit } from '@/_models';
+import { Audit,User } from '@/_models';
 import { AuditService, AuthenticationService } from '@/_services';
 
 @Component({ templateUrl: 'audit.component.html' })
 export class AuditComponent implements OnInit
 {
     audits = [];
-
+    currentUser: User;
     constructor(
         private authenticationService: AuthenticationService,
         private auditService: AuditService
     )
     {
+        this.currentUser = this.authenticationService.currentUserValue;
     }
 
     ngOnInit()
@@ -26,5 +27,20 @@ export class AuditComponent implements OnInit
         this.auditService.getAll()
             .pipe(first())
             .subscribe(audits => this.audits = audits);
+    }
+
+    private onOptionsSelected(val) {
+        if(val == "12") {
+            this.convertTo12();
+        }
+        else {
+            this.convertTo24();
+        }
+    }
+    convertTo12(){
+
+    }
+    convertTo24(){
+
     }
 }
